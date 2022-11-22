11bb28032577705d557b6138c1a5ffd94e747341
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-11-22 15:05:04 -0300

    routes added

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import GlobalStyled from './styles/global'
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<GlobalStyled />
-    	<App />
+		<BrowserRouter>
+			<GlobalStyled />
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 )
---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { Switch, Route } from 'react-router-dom'
+import { Dashboard } from '../pages/Dashboard'
+import { NotFound } from '../pages/NotFound'
+import { Home } from '../pages/Home'
+import React from 'react'
+
+
+const Routes = () => {
+    
+    return (
+        <Switch>
+            <Route path="/dashboard">
+                <Dashboard />
+            </Route>
+
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            <Route component={ NotFound } />
+        </Switch>
+    )
+}
+
+export { Routes }
