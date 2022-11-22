cf4fb6a1c03e52cc7a3b6969878b88618ddd14ea
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-11-22 17:02:43 -0300

    favicon added

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import GlobalStyled from './styles/global'
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-import App from './App'
+import { App } from './App'
 import { BrowserRouter } from 'react-router-dom'
 
 
