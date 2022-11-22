ce7b9aaee68decc9395db881ebddf721f3ce7f37
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-11-22 14:59:19 -0300

    added global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import GlobalStyled from './styles/global'
 import ReactDOM from 'react-dom/client'
+import React from 'react'
 import App from './App'
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<GlobalStyled />
+    	<App />
+	</React.StrictMode>
 )
